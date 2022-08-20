4c5bef2094c51e93a5ad1235e30f112789cc7ac3
Sriram Vaddadi <saisriram98gmail.com>
2022-08-20 21:58:00 +0530

    Added basic react project

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <div>
+      <h1 >Hi there! Welcome to Task Manager.</h1>
+    </div>
+  );
+};
+
+export default App;
